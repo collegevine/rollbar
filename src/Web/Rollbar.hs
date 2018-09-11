b79fbeba7303b92061e925d8942954c86210e61a
Daniel Gasienica <daniel@gasienica.ch>
2018-09-11 12:01:01 -0400

    Normalize formatting using `hindent`
    Command: `make format`

--- src/Web/Rollbar.hs
+++ src/Web/Rollbar.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Web.Rollbar(
-    module Web.Rollbar.Types,
-    rollbar
-) where
+module Web.Rollbar
+  ( module Web.Rollbar.Types
+  , rollbar
+  ) where
 
 import Web.Rollbar.Types
 
-import Control.Lens (view, (^.))
+import Control.Lens ((^.), view)
 import Control.Monad (when)
 import Control.Monad.Except (MonadError)
 import Control.Monad.Reader (MonadReader)
@@ -17,23 +17,40 @@ import Data.Char (toLower)
 import Data.Maybe (maybeToList)
 import Network.HTTP.Nano
 
-rollbar :: (MonadIO m, MonadError e m, MonadReader r m, AsHttpError e, HasHttpCfg r, HasRollbarCfg r, ToRollbarEvent evt) => evt -> m ()
+rollbar ::
+     ( MonadIO m
+     , MonadError e m
+     , MonadReader r m
+     , AsHttpError e
+     , HasHttpCfg r
+     , HasRollbarCfg r
+     , ToRollbarEvent evt
+     )
+  => evt
+  -> m ()
 rollbar evt = do
-    mute <- view rollbarCfgMute
-    when (not mute) $ do
-        v <- encodeEvent $ toRollbarEvent evt
-        http' . addHeaders [("Content-Type", "application/json")] =<< buildReq POST "https://api.rollbar.com/api/1/item/" (mkJSONData v)
+  mute <- view rollbarCfgMute
+  when (not mute) $ do
+    v <- encodeEvent $ toRollbarEvent evt
+    http' . addHeaders [("Content-Type", "application/json")] =<<
+      buildReq POST "https://api.rollbar.com/api/1/item/" (mkJSONData v)
 
 encodeEvent :: (MonadReader r m, HasRollbarCfg r) => Event -> m Value
 encodeEvent evt = do
-    tok <- view rollbarCfgToken
-    env <- view rollbarCfgEnvironment
-    return $ object ["access_token" .= tok, "data" .= dta env]
-    where
-    dta env =
-        object $ [
-            "environment" .= env,
-            "level" .= (toLower <$> show (evt ^. eventLevel)),
-            "title" .= (evt ^. eventTitle),
-            "body" .= object ["message" .= object ["body" .= (evt ^. eventMessage), "data" .= (evt ^. eventData)]]
-        ] ++ maybeToList (("uuid".=) <$> evt ^. eventUUID)
+  tok <- view rollbarCfgToken
+  env <- view rollbarCfgEnvironment
+  return $ object ["access_token" .= tok, "data" .= toData env]
+  where
+    toData env =
+      object $
+      [ "environment" .= env
+      , "level" .= (toLower <$> show (evt ^. eventLevel))
+      , "title" .= (evt ^. eventTitle)
+      , "body" .=
+        object
+          [ "message" .=
+            object
+              ["body" .= (evt ^. eventMessage), "data" .= (evt ^. eventData)]
+          ]
+      ] ++
+      maybeToList (("uuid" .=) <$> evt ^. eventUUID)
